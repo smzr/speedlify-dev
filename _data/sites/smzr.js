6d5ad34044700e9fe70a6ba5e2b0bfde903ab568
Sammy McKay <sammy@smzr.co.uk>
2022-04-23 16:14:57 +0100

    update timing

--- _data/sites/smzr.js
+++ _data/sites/smzr.js
@@ -1,15 +1,9 @@
 module.exports = {
 	name: "smzr.co.uk", // optional, falls back to object key
 	description: "Sammy McKay's personal website",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: false,
 	options: {
-		frequency: 60 * 23, // 23 hours
-		// Use "run" if the sites don’t share assets on the same origin
-		//           and we can reset chrome with each run instead of
-		//           each site in every run (it’s faster)
-		// Use "site" if sites are all on the same origin and share assets.
+		frequency: 60 * 24, // 24 hours
 		freshChrome: "site"
 	},
 	urls: [
