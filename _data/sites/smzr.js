4191aa468e5b438df54622aabe9fa4ccc7d1c24f
Sammy McKay <sammy@smzr.co.uk>
2022-04-26 21:14:43 +0100

    Update urls

--- _data/sites/smzr.js
+++ _data/sites/smzr.js
@@ -3,17 +3,17 @@ module.exports = {
 	description: "Sammy McKay's personal website",
 	skip: false,
 	options: {
-		frequency: 60 * 23, // 23 hours
+		frequency: 60, // 1 hours
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.smzr.co.uk/",
-		"https://www.smzr.co.uk/projects/",
-		"https://www.smzr.co.uk/snippets/",
-		"https://www.smzr.co.uk/blog/",
-		"https://www.smzr.co.uk/contact/",
+		"https://smzr.co.uk/",
+		"https://smzr.co.uk/projects/",
+		"https://smzr.co.uk/snippets/",
+		"https://smzr.co.uk/blog/",
+		"https://smzr.co.uk/contact/",
 		// Popular Posts
-		"https://www.smzr.co.uk/blog/yet-another-website-rebuild-11ty/",
-		"https://www.smzr.co.uk/blog/adding-netlify-cms-to-11ty/",
+		"https://smzr.co.uk/blog/yet-another-website-rebuild-11ty/",
+		"https://smzr.co.uk/blog/adding-netlify-cms-to-11ty/",
 	]
 };
